52eb12fdc20db0d4b4030f20375ec4cf0ef197ca
asbonato <asbonato@gmail.com>
2024-05-11 10:20:14 -0300

    microsservicos(ecm516): mss barramento-de-eventos - até pag. 42

--- ecm516/app_lembretes/lembretes/index.js
+++ ecm516/app_lembretes/lembretes/index.js
@@ -1,4 +1,5 @@
-const express = require('express');
+const express = require('express')
+const axios = require('axios')
 const app = express();
 app.use(express.json());
 
@@ -9,13 +10,25 @@ app.get('/lembretes', (req, res) => {
   res.send(lembretes);
 })
 
-app.post('/lembretes', (req, res) => {
+app.post('/lembretes', async (req, res) => {
     const { texto } = req.body;
     contador++;
     lembretes[contador] = {
       contador, texto
     }
+  await axios.post("http://localhost:10000/eventos", {
+    tipo: 'LembreteCriado',
+    dados: {
+      contador,
+      texto
+    }
+  })
   res.status(201).send(lembretes[contador]);
+})
+
+app.post('/eventos', (req, res) => {
+  console.log(req.body)
+  res.status(200).send({msg: 'ok'})
 })
 
 app.listen(4000, () => {
