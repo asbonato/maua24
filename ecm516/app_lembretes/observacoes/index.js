bceaa5489b31fef8bcbbd1666b4ec8948774c69b
asbonato <asbonato@gmail.com>
2024-05-25 10:22:22 -0300

    microsservicos(ecm516): aula 25/05 - mss classificacao finalizado - pag. 63

--- ecm516/app_lembretes/observacoes/index.js
+++ ecm516/app_lembretes/observacoes/index.js
@@ -6,6 +6,26 @@ app.use(express.json());
 
 const observacoesPorLembreteId = {};
 
+const funcoes = {
+  ObservacaoClassificada: (observacao) => {
+    const observacoes = 
+      observacoesPorLembreteId[observacao.lembreteId]
+    const obsParaAtualizar = 
+      observacoes.find(obs => obs.id === observacao.id)
+    obsParaAtualizar.status = observacao.status
+    axios.post('http://localhost:10000/eventos', {
+      tipo: 'ObservacaoAtualizada',
+      dados: {
+        id: observacao.id,
+        texto: observacao.texto,
+        lembreteId: observacao.lembreteId,
+        status: observacao.status
+      }
+    })
+
+  }
+}
+
 
 //:id é um placeholder
 //exemplo: /lembretes/1/observacoes
@@ -14,14 +34,15 @@ app.post('/lembretes/:id/observacoes', async (req, res) => {
   const { texto } = req.body;
   //req.params dá acesso à lista de parâmetros da URL
   const observacoesDoLembrete = observacoesPorLembreteId[req.params.id] || [];
-  observacoesDoLembrete.push({ id: idObs, texto });
+  observacoesDoLembrete.push({ id: idObs, texto, status: 'aguardando' });
   observacoesPorLembreteId[req.params.id] = observacoesDoLembrete;
   await axios.post('http://localhost:10000/eventos', {
     tipo: "ObservacaoCriada",
     dados: {
       id: idObs,
       texto,
-      lembreteId: req.params.id
+      lembreteId: req.params.id,
+      status: 'aguardando'
     }
   })
   res.status(201).send(observacoesDoLembrete);
@@ -32,7 +53,9 @@ app.get('/lembretes/:id/observacoes', (req, res) => {
 })
 
 app.post('/eventos', (req, res) => {
-  console.log(req.body)
+  try{
+    funcoes[req.body.tipo](req.body.dados)
+  }catch(err){}
   res.status(200).send({msg: 'ok'})
 })
 
