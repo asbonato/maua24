2385dfc79de22a17fc927646fad944bfb64b92b9
asbonato <asbonato@gmail.com>
2024-08-31 11:20:49 -0300

    apostila2(dart): ecm252 - aula 31/8 - finalizada aplicação pedra-papel-tesoura terminada

--- ecm252/pedra_papel_tesoura/lib/pedra_papel_tesoura.dart
+++ ecm252/pedra_papel_tesoura/lib/pedra_papel_tesoura.dart
@@ -1,3 +1,62 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:io';
+import 'dart:math';
+
+enum OPCAO { pedra, papel, tesoura}
+
+void exibe(String texto){
+  print(texto);
+}
+
+int pegaOpcaoUsuario() {
+  return int.parse(stdin.readLineSync()!);
+}
+
+bool opcaoEhValida(int opcao){
+  return opcao >= 1 && opcao <= 4;
+}
+
+OPCAO mapeiaOpcao(int opcao){
+  return OPCAO.values[opcao - 1];
+}
+
+String decideResultado(OPCAO opcaoUsuario, OPCAO opcaoComputador){
+  if (opcaoUsuario == opcaoComputador) return "Empate";
+  if ((opcaoUsuario == OPCAO.papel && opcaoComputador == OPCAO.pedra) ||
+    (opcaoUsuario == OPCAO.pedra && opcaoComputador == OPCAO.tesoura) ||
+    (opcaoUsuario == OPCAO.tesoura && opcaoComputador == OPCAO.papel)) {
+      return "Você venceu";
+    }
+  return "Computador venceu";
+}
+
+void jogo(){
+  int opUsuario;
+  do{
+    //exibir menu
+
+    //capturar opção do usuário, validando
+    do{
+      exibe('1-Pedra\n2-Papel\n3-Tesoura\n4-Sair');
+      opUsuario = pegaOpcaoUsuario();
+    }while(!opcaoEhValida(opUsuario));
+    //se o usuário quer sair, digitando 4
+    //senão
+    if (opUsuario != 4){
+      //sortear a escolha do computador
+      int opComputador = Random().nextInt(3) + 1;
+      //mapear opcao usuario, de int para enum
+      OPCAO opcaoUsuario = mapeiaOpcao(opUsuario);
+      //mapear opecao computador, de int para enum
+      OPCAO opcaoComputador = mapeiaOpcao(opComputador);
+      //exibir opções de cada um
+      exibe('Você (${opcaoUsuario.name}) vs (${opcaoComputador.name}) Computador');
+      //decidir quem venceu ou se houve empate
+      String vencedor = decideResultado(opcaoUsuario, opcaoComputador);
+      exibe(vencedor);
+      exibe('*'*50);
+      //exibir o resultado
+      sleep(Duration(seconds: 3));
+    }
+  }while(opUsuario != 4);
+  exibe('Até logo');
 }
--- ecm252/pedra_papel_tesoura/test/pedra_papel_tesoura_test.dart
+++ ecm252/pedra_papel_tesoura/test/pedra_papel_tesoura_test.dart
@@ -2,7 +2,22 @@ import 'package:pedra_papel_tesoura/pedra_papel_tesoura.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('opcaoEhValida', () {
+    expect(opcaoEhValida(1), true);
+  });
+  test('opcaoEhValida', () {
+    expect(opcaoEhValida(2), true);
+  });
+  test('opcaoEhValida', () {
+    expect(opcaoEhValida(3), true);
+  });
+  test('opcaoEhValida', () {
+    expect(opcaoEhValida(4), true);
+  });
+  test('opcaoEhValida', () {
+    expect(opcaoEhValida(0), false);
+  });
+  test('opcaoEhValida', () {
+    expect(opcaoEhValida(5), false);
   });
 }
